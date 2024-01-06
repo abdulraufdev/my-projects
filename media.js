12d087c0c408271a4247790659a5efef42974aea
Abdul Rauf <editsabdulrauf5681@gmail.com>
2024-01-06 17:40:28 +0500

    pics updated

--- media.js
+++ media.js
@@ -1,18 +1,18 @@
-let proj5 = document.getElementById("project--5");
-let proj4 = document.getElementById("project--4");
-let proj3 = document.getElementById("project--3");
-let proj2 = document.getElementById("project--2");
 let proj1 = document.getElementById("project--1");
+let proj2 = document.getElementById("project--2");
+let proj3 = document.getElementById("project--3");
+let proj4 = document.getElementById("project--4");
+let proj5 = document.getElementById("project--5");
 
 let proj6 = document.getElementById("project--6");
 let proj7 = document.getElementById("project--7");
 let proj8 = document.getElementById("project--8");
 
-proj1.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/melodica_thumb.png)');
+proj1.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/melodica_thumb.png)');
 proj2.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumbnail.png)');
 proj3.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/ades_thumb.png)');
 proj4.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/am_1.png)');
-proj5.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumb_penpot.png)');
-proj6.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/framer_thumb.png)');
-proj7.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumb.png)');
+proj5.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/penpot_thumb.png)');
+proj6.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/framer_thumb.png)');
+proj7.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/andr15_thumb.png)');
 proj8.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/supercook_thumb.png)');
