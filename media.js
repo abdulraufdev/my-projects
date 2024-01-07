d7e597f2564a825c050543afe303d0f85ffaa4ed
Abdul Rauf <editsabdulrauf5681@gmail.com>
2024-01-07 09:17:06 +0500

    updated imgs

--- media.js
+++ media.js
@@ -7,12 +7,14 @@ let proj5 = document.getElementById("project--5");
 let proj6 = document.getElementById("project--6");
 let proj7 = document.getElementById("project--7");
 let proj8 = document.getElementById("project--8");
+let proj9 = document.getElementById("project--9");
 
 proj1.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/melodica_thumb.png)');
 proj2.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumbnail.png)');
-proj3.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/ades_thumb.png)');
+proj3.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/adesar_thumb.png?updatedAt=1704600342033)');
 proj4.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/am_1.png)');
 proj5.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/penpot_thumb.png)');
 proj6.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/framer_thumb.png)');
 proj7.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/andr15_thumb.png)');
 proj8.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/supercook_thumb.png)');
+proj9.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/mathtrove_thumb.png?updatedAt=1704599436888)');
