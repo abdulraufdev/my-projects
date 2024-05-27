73b726ff76154179038ba104270f8156097a5c63
Abdul Rauf <editsabdulrauf5681@gmail.com>
2024-05-26 23:13:56 -0700

    media updated

--- media.js
+++ media.js
@@ -1,20 +1,9 @@
-let proj1 = document.getElementById("project--1");
-let proj2 = document.getElementById("project--2");
-let proj3 = document.getElementById("project--3");
-let proj4 = document.getElementById("project--4");
-let proj5 = document.getElementById("project--5");
 
-let proj6 = document.getElementById("project--6");
-let proj7 = document.getElementById("project--7");
-let proj8 = document.getElementById("project--8");
-let proj9 = document.getElementById("project--9");
+let projImgs = document.querySelectorAll('.proj-img');
 
-proj1.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/melodica_thumb.png)');
-proj2.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumbnail.png)');
-proj3.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/adesar_thumb.png?updatedAt=1704600342033)');
-proj4.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/am_1.png)');
-proj5.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/penpot_thumb.png)');
-proj6.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/framer_thumb.png)');
-proj7.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/andr15_thumb.png)');
-proj8.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/supercook_thumb.png)');
-proj9.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/pfolioMe/mathtrove_thumb.png?updatedAt=1704599436888)');
+let projectImgs = ["https://ik.imagekit.io/abduledits5/pfolioMe/ascend_thumb.png?updatedAt=1716746945751","https://ik.imagekit.io/abduledits5/pfolioMe/mathtrove_thumb.png?updatedAt=1716738142375","https://ik.imagekit.io/abduledits5/pfolioMe/supercook_thumb.png?updatedAt=1716735538741", "https://ik.imagekit.io/abduledits5/pfolioMe/andr15_thumb.png?updatedAt=1716735500016", "https://ik.imagekit.io/abduledits5/pfolioMe/framer_thumb.png?updatedAt=1716736659216", "https://ik.imagekit.io/abduledits5/pfolioMe/penpot_thumb.png?updatedAt=1716735465871", "https://ik.imagekit.io/abduledits5/pfolioMe/am_thumb.png?updatedAt=1716736181494", "https://ik.imagekit.io/abduledits5/pfolioMe/adesar_thumb.png?updatedAt=1716736241152","https://ik.imagekit.io/abduledits5/pfolioMe/win12_thumb.png?updatedAt=1716737774479", "https://ik.imagekit.io/abduledits5/pfolioMe/melodica_thumb.png?updatedAt=1716722854399"]
+let projColors = ["#2a2ea7","#111111","#2f4b21", "#006465", "#003f65","#005d4a","#161c30","#39352d","#010a18","#0e2351"];
+for (let i = 0; i < projectImgs.length; i++) {
+    projImgs[i].style.backgroundImage = `url(${projectImgs[i]})`;
+    projImgs[i].parentElement.style.backgroundColor = projColors[i];
+}
